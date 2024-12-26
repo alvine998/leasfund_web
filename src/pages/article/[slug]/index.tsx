bd89b746494f2d2fb44bb2f657b49613cc641bd6
alvin <alvinecom2018@gmail.com>
2024-12-26 11:59:41 +0700

    set google aanlytics & property domain

--- src/pages/article/[slug]/index.tsx
+++ src/pages/article/[slug]/index.tsx
@@ -1,13 +1,15 @@
 import BottomNav from "@/components/BottomNav";
 import DetailArticle from "@/components/DetailArticle";
 import Navbar from "@/components/Navbar";
-import {  getArticleBySlug } from "@/pages/api/article";
-import { useParams } from "next/navigation";
+import { getArticleBySlug } from "@/pages/api/article";
+import Head from "next/head";
+import { useParams, usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 export default function DetailArticlePage() {
   const [detail, setDetail] = useState<any>();
   const params: any = useParams();
+  const pathname = usePathname();
 
   const getDetail = async () => {
     try {
@@ -24,6 +26,30 @@ export default function DetailArticlePage() {
   }, []);
   return (
     <div>
+      <Head>
+        {/* Basic SEO Meta Tags */}
+        <title>{detail?.title}</title>
+        <meta name="description" content={detail?.description} />
+        <meta name="author" content={"admin leasfund"} />
+        <meta name="keywords" content={detail?.keywords} />
+        <link rel="canonical" href={pathname} />
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={detail?.title} />
+        <meta property="og:description" content={detail?.description} />
+        <meta property="og:image" content={detail?.thumbnail} />
+        <meta property="og:url" content={pathname} />
+        <meta property="article:author" content={"admin leasfund"} />
+        <meta property="article:published_time" content={detail?.createdAt} />
+
+        {/* Twitter Card Meta Tags */}
+        <meta name="twitter:card" content={detail?.thumbnail} />
+        <meta name="twitter:title" content={detail?.title} />
+        <meta name="twitter:description" content={detail?.description} />
+        <meta name="twitter:image" content={detail?.thumbnail} />
+        <meta name="twitter:creator" content={"admin leasfund"} />
+      </Head>
       <Navbar />
       <div className="">
         <DetailArticle detail={detail} />
